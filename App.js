9f40d59cfc6dea644d5808b3739eec71e456e7c3
Scott Taylor <scott@sudocats.com>
2022-12-22 09:08:56 -0800

    modal view example

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ import * as imagePicker from 'expo-image-picker';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiiPicker from './components/EmojiiPicker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
 
   const pickImageAsync = async () => {
@@ -19,11 +24,28 @@ export default function App() {
 
     if( !result.canceled ) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select an image');
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // todo
+  };
+  
   return (
     <View style={styles.container}>
 
@@ -36,10 +58,24 @@ export default function App() {
           />
       </View>
 
-      <View style={styles.footerContainer} >
-        <Button theme="primary" label="Test Button" onPress={pickImageAsync} />
-        <Button label="Log In" />
-      </View>
+      { showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton icon="refresh" label="Reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+            </View>
+          </View>
+      ) : (
+          <View style={styles.footerContainer} >
+            <Button theme="primary" label="Test Button" onPress={pickImageAsync} />
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+          </View>
+      )}
+
+      <EmojiiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* list of emojii here */}
+      </EmojiiPicker>
 
       <StatusBar style="auto" />
     </View>
@@ -61,5 +97,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
